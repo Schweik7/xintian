970828050868636d07b7c5478b631541d657fc0d
Schweik7 <weibidewo0@gmail.com>
2024-08-12 09:19:19 +0800

    学会了正确导入图片

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,18 +3,18 @@
 <template>
   <header class="header">
     <div class="header-container">
-      <img class="logo" src="../images/f8041d2cbf13399592077834bc083483.png" alt="Logo" />
+      <img class="logo" src="@/images/f8041d2cbf13399592077834bc083483.png" alt="Logo" />
       <div class="search-box">
         <input type="text" placeholder="请输入搜索关键词" />
-        <img src="../images/4c1360ec6cb043bb5fcd6ff4f5faf82b.png" alt="Search" />
+        <img src="@/images/4c1360ec6cb043bb5fcd6ff4f5faf82b.png" alt="Search" />
       </div>
       <div class="header-actions">
         <div class="header-action">
-          <img src="../images/c0a51bcf32621e352243a0d7f22b176c.png" alt="Help" />
+          <img src="@/images/c0a51bcf32621e352243a0d7f22b176c.png" alt="Help" />
           <span>帮助</span>
         </div>
         <div class="header-action">
-          <img src="../images/e8eaae0dfcb172501d4660c699e24765.png" alt="Login" />
+          <img src="@/images/e8eaae0dfcb172501d4660c699e24765.png" alt="Login" />
           <span>登录</span>
         </div>
       </div>
--- src/components/YourTroubles.vue
+++ src/components/YourTroubles.vue
@@ -1,43 +1,51 @@
 <!-- src/components/YourTroubles.vue -->
+
 <template>
     <section class="your-troubles">
-      <div class="trouble-category" v-for="category in categories" :key="category.title">
-        <img :src="category.image" :alt="category.title" />
-        <h3>{{ category.title }}</h3>
-        <p>{{ category.description }}</p>
-      </div>
+        <div class="trouble-category" v-for="category in categories" :key="category.title">
+            <img :src="category.image" :alt="category.title" />
+            <h3>{{ category.title }}</h3>
+            <p>{{ category.description }}</p>
+        </div>
     </section>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+import image1 from '../images/4cecb4b29b99eeb8e0f7de4b868bc99b.png';
+import image2 from '../images/07f3b77435693004758d44a2edb3c8f2.png';
+import image3 from '../images/ec27304e906837ba42e2b29dd404651a.png';
+import image4 from '../images/93dcf3e14b22cec8d38daeffadb15f79.png';
+export default {
     name: 'YourTroubles',
     data() {
-      return {
-        categories: [
-          {
-            title: '儿童青少年',
-            description: '意志行为障碍、行为障碍问题、情绪发展问题、社会性交往',
-            image: '../images/4cecb4b29b99eeb8e0f7de4b868bc99b.png',
-          },
-          {
-            title: '亲子关系',
-            description: '与父母的沟通、亲子关系单薄、对父母的态度、父母的压力',
-            image: '../images/07f3b77435693004758d44a2edb3c8f2.png',
-          },
-          {
-            title: '人际交往',
-            description: '社会关系、师生关系、沟通交流、孤独自卑',
-            image: '../images/ec27304e906837ba42e2b29dd404651a.png',
-          },
-          {
-            title: '个人成长',
-            description: '学业压力、情绪困扰、自我评价',
-            image: '../images/93dcf3e14b22cec8d38daeffadb15f79.png',
-          },
-        ],
-      };
+        return {
+            categories: [
+                {
+                    title: '儿童青少年',
+                    description: '意志行为障碍、行为障碍问题、情绪发展问题、社会性交往',
+                    // image: '@/images/4cecb4b29b99eeb8e0f7de4b868bc99b.png',
+                    image: image1,
+                },
+                {
+                    title: '亲子关系',
+                    description: '与父母的沟通、亲子关系单薄、对父母的态度、父母的压力',
+                    // image: '@/images/07f3b77435693004758d44a2edb3c8f2.png',
+                    image: image2,
+                },
+                {
+                    title: '人际交往',
+                    description: '社会关系、师生关系、沟通交流、孤独自卑',
+                    // image: '@/images/ec27304e906837ba42e2b29dd404651a.png',
+                    image: image3,
+                },
+                {
+                    title: '个人成长',
+                    description: '学业压力、情绪困扰、自我评价',
+                    // image: '@/images/93dcf3e14b22cec8d38daeffadb15f79.png',
+                    image: image4,
+                },
+            ],
+        };
     },
-  };
-  </script>
-  
+};
+</script>
