e7f27cedf692fb23e169a9d6f20fe5668b0e5ede
Schweik7 <weibidewo0@gmail.com>
2024-08-12 08:50:21 +0800

    加入对App.vue的修改

--- src/App.vue
+++ src/App.vue
@@ -1,235 +1,29 @@
+<!-- App.vue -->
 <template>
-    <div class="page flex-col">
-        <div class="box_1 flex-col">
-            <div class="section_1 flex-row">
-                <img class="image_1" referrerpolicy="no-referrer" src="./images/f8041d2cbf13399592077834bc083483.png" />
-                <div class="box_2 flex-row justify-between">
-                    <span class="text_1">请输入搜索关键词</span>
-                    <img class="label_1" referrerpolicy="no-referrer"
-                        src="./images/4c1360ec6cb043bb5fcd6ff4f5faf82b.png" />
-                </div>
-                <div class="box_3 flex-row">
-                    <img class="thumbnail_1" referrerpolicy="no-referrer"
-                        src="./images/c0a51bcf32621e352243a0d7f22b176c.png" />
-                    <span class="text_2">帮助</span>
-                </div>
-                <div class="box_4 flex-row">
-                    <img class="thumbnail_2" referrerpolicy="no-referrer"
-                        src="./images/e8eaae0dfcb172501d4660c699e24765.png" />
-                    <span class="text_3">登录</span>
-                </div>
-            </div>
-            <div class="section_2 flex-col">
-                <div class="group_1 flex-col"></div>
-                <div class="text-wrapper_11 flex-row justify-between">
-                    <span class="text_4">心灵探索馆</span>
-                    <span class="text_5">身心研究室</span>
-                    <span class="text_6">心灵蜕变驿站</span>
-                </div>
-            </div>
-            <div class="section_14 flex-row justify-between">
-                <div class="box_13 flex-col">
-                    <span
-                        class="text_7">欢迎来到&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;网站</span>
-                    <div class="block_1 flex-col"></div>
-                    <div class="text-wrapper_2 flex-col">
-                        <span class="text_in_button">了解更多</span>
-                    </div>
-                    <span class="text_9">一个帮助你心理成长的空间</span>
-                </div>
-                <img class="image_2" referrerpolicy="no-referrer" src="./images/e301364da532ea7e1909f99fc017c855.png" />
-            </div>
-            <div class="section_4 flex-col">
-                <div class="group_9 flex-row justify-between">
-                    <span class="text_10">心灵蜕变驿站</span>
-                    <div class="box_14 flex-col">
-                        <div class="box_5 flex-col"></div>
-                        <span class="text_11">心灵探索馆</span>
-                    </div>
-                    <span class="text_12">身心研究所</span>
-                </div>
-                <div class="group_10 flex-row justify-end">
-                    <div class="block_2 flex-col">
-                        <div class="group_11 flex-row justify-between">
-                            <img class="label_2" referrerpolicy="no-referrer"
-                                src="./images/6afb9b2045608dd4a2019b3d7ec8ff90.png" />
-                            <span class="text_13">心理学堂</span>
-                        </div>
-                        <span
-                            class="paragraph_1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在这里，用户可以接触到丰富的心理学知识，包<br />括心理发展的基础知识、常见心理问题的原因、症状<br />以及对应策略。</span>
-                        <div class="text-wrapper_3 flex-col">
-                            <span class="text_in_button">立即体验</span>
-                        </div>
-                    </div>
-                    <div class="block_3 flex-col">
-                        <div class="section_15 flex-row justify-between">
-                            <img class="label_3" referrerpolicy="no-referrer"
-                                src="./images/f1e6ecdcea4be927593341a1cea33963.png" />
-                            <span class="text_15">量表测试</span>
-                        </div>
-                        <span
-                            class="paragraph_2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;提供经典心理测评问卷，如：抑郁症、焦虑症、<br />强迫症等，帮助用户了解自身心理状况。<br /></span>
-                        <div class="text-wrapper_4 flex-col">
-                            <span class="text_in_button">立即体验</span>
-                        </div>
-                        <img class="image_3" referrerpolicy="no-referrer"
-                            src="./images/bdb2a4a850f75eb2f403a1b209af66fb.png" />
-                    </div>
-                    <div class="block_4 flex-col">
-                        <div class="box_15 flex-row justify-between">
-                            <img class="image_4" referrerpolicy="no-referrer"
-                                src="./images/fe741c7a583bb2e08bd5784d9c990776.png" />
-                            <span class="text_in_button">VR测试</span>
-                        </div>
-                        <span
-                            class="paragraph_3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;自主研发VR评估场景（如情景测验）等，提高<br />测评的信效度，减轻心理老师等相关工作者的工作<br />压力。<br /></span>
-                        <div class="text-wrapper_5 flex-col">
-                            <span class="text_in_button">立即体验</span>
-                        </div>
-                    </div>
-                    <div class="image-wrapper_1 flex-col">
-                        <img class="thumbnail_3" referrerpolicy="no-referrer"
-                            src="./images/383023060211e355270f215b06af9358.png" />
-                    </div>
-                    <div class="image-wrapper_2 flex-col">
-                        <img class="thumbnail_4" referrerpolicy="no-referrer"
-                            src="./images/872ad0740b17e68d2451d234fd483a6f.png" />
-                    </div>
-                </div>
-                <div class="image-wrapper_4 flex-row justify-between">
-                    <img class="image_5" referrerpolicy="no-referrer"
-                        src="./images/2c255bd8a37885d892156609de8bc6cd.png" />
-                    <img class="image_6" referrerpolicy="no-referrer"
-                        src="./images/7a15b93dca0b0ab792b7a38a4af5147a.png" />
-                </div>
-            </div>
-            <div class="section_5 flex-col"></div>
-            <div class="section_16 flex-row justify-between">
-                <div class="block_5 flex-col">
-                    <div class="image-text_9 flex-row justify-between">
-                        <img class="label_4" referrerpolicy="no-referrer"
-                            src="./images/4cecb4b29b99eeb8e0f7de4b868bc99b.png" />
-                        <span class="text-group_1">儿童青少年</span>
-                    </div>
-                    <span class="paragraph_4">意志行为障碍<br />行为障碍问题<br />情绪发展问题<br />社会性交往</span>
-                </div>
-                <div class="block_6 flex-col">
-                    <div class="image-text_10 flex-row justify-between">
-                        <img class="label_5" referrerpolicy="no-referrer"
-                            src="./images/07f3b77435693004758d44a2edb3c8f2.png" />
-                        <span class="text-group_2">亲子关系</span>
-                    </div>
-                    <span class="paragraph_5">与父母的沟通<br />亲子关系单薄<br />对父母的态度<br />父母的压力</span>
-                </div>
-                <div class="block_7 flex-col">
-                    <div class="image-text_11 flex-row justify-between">
-                        <img class="label_6" referrerpolicy="no-referrer"
-                            src="./images/ec27304e906837ba42e2b29dd404651a.png" />
-                        <span class="text-group_3">人际交往</span>
-                    </div>
-                    <span class="paragraph_6">社会关系<br />师生关系<br />沟通交流<br />孤独自卑</span>
-                </div>
-                <div class="block_8 flex-col">
-                    <div class="image-text_12 flex-row justify-between">
-                        <img class="label_7" referrerpolicy="no-referrer"
-                            src="./images/93dcf3e14b22cec8d38daeffadb15f79.png" />
-                        <span class="text-group_4">个人成长</span>
-                    </div>
-                    <span class="paragraph_7">学业压力<br />情绪困扰<br />自我评价<br /></span>
-                </div>
-            </div>
-            <div class="section_7 flex-col"></div>
-            <div class="section_8 flex-col"></div>
-            <div class="section_17 flex-row justify-between">
-                <div class="group_5 flex-col"></div>
-                <div class="group_12 flex-col justify-between">
-                    <div class="para-wrapper_6">
-                        <span class="text_19">北京师范大学、北京林业大学</span>
-                        <span class="paragraph_8">为代表的研究团队提供青少年心理评估与干预的理论和实践依据<br /></span>
-                        <span class="text_20">上海市浦东新区精神卫生中心</span>
-                        <span class="paragraph_9">为主体的临床团队提供临床经验和实地测验证据<br /></span>
-                        <span class="text_21">中国信通院江西研究院研发团队</span>
-                        <span class="text_22">提供专业数字化技术和产业基础保障</span>
-                    </div>
-                    <div class="text-wrapper_7 flex-col">
-                        <span class="text_in_button">进一步了解</span>
-                    </div>
-                </div>
-            </div>
-            <div class="section_10 flex-col"></div>
-            <div class="section_11 flex-col"></div>
-            <div class="section_18 flex-row justify-between">
-                <div class="box_9 flex-row">
-                    <div class="image-text_13 flex-row justify-between">
-                        <img class="label_8" referrerpolicy="no-referrer"
-                            src="./images/61a7ab2b04175497bc80d41be73381dc.png" />
-                        <span class="text-group_5">简单心理</span>
-                    </div>
-                </div>
-                <div class="box_10 flex-row">
-                    <div class="image-text_14 flex-row justify-between">
-                        <img class="label_9" referrerpolicy="no-referrer"
-                            src="./images/deca01bfe8cf3956e0379b7191477fec.png" />
-                        <span class="text-group_6">壹点灵</span>
-                    </div>
-                </div>
-                <div class="box_11 flex-row">
-                    <div class="image-text_15 flex-row justify-between">
-                        <img class="label_10" referrerpolicy="no-referrer"
-                            src="./images/6b24b78ed6ac960fc37cd77bbd6f08f3.png" />
-                        <span class="text-group_7">壹心理</span>
-                    </div>
-                </div>
-                <div class="box_12 flex-row">
-                    <div class="image-text_16 flex-row justify-between">
-                        <img class="label_11" referrerpolicy="no-referrer"
-                            src="./images/3de7074e5ae724090b4a0e034491e5f8.png" />
-                        <span class="text-group_8">阳光易德</span>
-                    </div>
-                </div>
-            </div>
-            <img class="image_7" referrerpolicy="no-referrer" src="./images/8bcc87ed5ef6d39955ef99074dd2f8df.png" />
-            <span class="text_24">你的困扰</span>
-            <img class="image_8" referrerpolicy="no-referrer" src="./images/8a44b99755cf4086bbb142cc89b87f85.png" />
-            <span class="text_25">我们的团队</span>
-            <span class="text_26">友情链接</span>
-            <div class="section_13 flex-col">
-                <div class="text-wrapper_12 flex-row justify-between">
-                    <span class="text_27">导航</span>
-                    <span class="text_28">社交媒体</span>
-                    <span class="text_29">联系我们</span>
-                </div>
-                <div class="text-wrapper_13 flex-row">
-                    <span class="text_30">首页</span>
-                    <span class="text_31">Facebook</span>
-                    <span class="text_32">Email:</span>
-                </div>
-                <div class="text-wrapper_14 flex-row">
-                    <span class="text_33">心灵探索馆</span>
-                    <span class="text_34">Youtube</span>
-                    <span class="text_35">Phone:&nbsp;XXX-XXXX-XXXX</span>
-                </div>
-                <span class="text_36">心理研究室</span>
-                <span class="text_37">心理蜕变驿站</span>
-                <div class="block_9 flex-col"></div>
-                <div class="section_19 flex-row">
-                    <span class="text_38">&#64;&nbsp;2024&nbsp;心田网站。专注于心理健康和个人成长。</span>
-                    <div class="group_7 flex-col"></div>
-                    <div class="group_8 flex-col"></div>
-                </div>
-            </div>
-        </div>
+    <div id="app">
+      <Header />
+      <MainSection />
+      <YourTroubles />
+      <OurTeam />
+      <Footer />
     </div>
-</template>
-<script>
-export default {
-    data() {
-        return {
-            constants: {},
-        };
+  </template>
+  
+  <script>
+  import Header from './components/Header.vue';
+  import MainSection from './components/MainSection.vue';
+  import YourTroubles from './components/YourTroubles.vue';
+  import OurTeam from './components/OurTeam.vue';
+  import Footer from './components/Footer.vue';
+  
+  export default {
+    components: {
+      Header,
+      MainSection,
+      YourTroubles,
+      OurTeam,
+      Footer,
     },
-    methods: {},
-};
-</script>
-<!-- <style src="./index.css" />
-<style src="./common.css" /> -->
+  };
+  </script>
+  
